c9da8960e26bdca36d36c2582a6e9ae6fddeb6ba
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2021-12-14 23:17:40 -0300

    in context, challenges, level, experience dinamic

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,24 +3,26 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
-  const level = useContext(ChallengesContext);
-  console.log(level)
+  const { activeChallenges, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenges ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenges.amount} xp</header>
 
           <main>
-            <img src='icons/body.svg' alt='' />
+            <img src={`icons/${activeChallenges.type}.svg`} />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenges.description}</p>
           </main>
 
           <footer>
-            <button type='button' className={styles.challengeFailedButton}>
+            <button
+              type='button'
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}
+            >
               Falhei
             </button>
             <button type='button' className={styles.challengeSucceededButton}>
--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,13 @@
-import styles from '../styles/components/CompletedChallenges.module.css'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{ level }</span>
     </div>
-  )
+  );
 }
