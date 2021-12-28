f1cc90f7d88d573edf9540cb7fed5639f4d99607
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2021-12-28 12:54:14 -0300

    adding sass e updates

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,26 @@
-import { useContext } from 'react'
-import { ChallengesContext } from '../contexts/ChallengesContext'
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from 'react';
+import { RiSettings5Fill } from 'react-icons/ri';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.scss';
 
-export function Profile(){
-  const { level } = useContext(ChallengesContext)
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/lucasAlmeidaSilveira.png" alt="Lucas Almeida" />
+      <img
+        src='https://github.com/lucasAlmeidaSilveira.png'
+        alt='Lucas Almeida'
+      />
       <div>
         <strong>Lucas Almeida</strong>
         <p>
-          <img src="icons/level.svg" alt="Level" />
+          <img src='icons/level.svg' alt='Level' />
           Level {level}
         </p>
       </div>
+      <button type='button'>
+        <RiSettings5Fill />
+      </button>
     </div>
-  )
+  );
 }
