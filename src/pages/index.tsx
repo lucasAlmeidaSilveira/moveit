6f973c8b7a5ca82da03d0eb2200559a72c9f01a9
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2021-12-28 13:02:58 -0300

    add props HomeProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ interface HomeProps {
   challengesCompleted: number;
 }
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
   return (
     <ChallengesProvider
       level={props.level}
